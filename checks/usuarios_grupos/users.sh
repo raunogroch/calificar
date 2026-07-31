ecc49e3878dcab1f1a02346464446753436e5965
Raul Noguera Ubuntu Imac <raul.noguera.rocha@gmail.com>
2026-07-31 12:01:26 -0400

    verificacion de cambios

--- checks/usuarios_grupos/users.sh
+++ checks/usuarios_grupos/users.sh
@@ -13,39 +13,32 @@ checks_per_user=2
 total_checks=$((total_users * checks_per_user))
 passed_checks=0
 
-echo "Validando existencia de usuarios:"
 for pair in "$@"; do
   user=${pair%%:*}
+  req_group=${pair#*:}
+  echo "Validando usuario: $user"
+
   if getent passwd "$user" >/dev/null; then
     echo " - Usuario existe: $user"
     passed_checks=$((passed_checks+1))
   else
     echo " - Usuario NO existe: $user"
-  fi
-done
-
-echo ""
-echo "Validando pertenencia a grupo:"
-for pair in "$@"; do
-  user=${pair%%:*}
-  req_group=${pair#*:}
-
-  if ! getent passwd "$user" >/dev/null; then
-    echo " - Omite grupo para $user porque el usuario no existe"
+    echo ""
     continue
   fi
 
   if ! getent group "$req_group" >/dev/null; then
     echo " - Grupo esperado NO existe: $req_group"
   elif id -nG "$user" | grep -qw "$req_group"; then
-    echo " - Usuario $user pertenece correctamente al grupo $req_group"
+    echo " - Usuario pertenece correctamente al grupo $req_group"
     passed_checks=$((passed_checks+1))
   else
-    echo " - Usuario $user NO pertenece al grupo $req_group"
+    echo " - Usuario NO pertenece al grupo $req_group"
   fi
+
+  echo ""
 done
 
-echo ""
 points=$(( passed_checks * FULL_POINTS / total_checks ))
 echo "POINTS:$points"
 if (( points == FULL_POINTS )); then
